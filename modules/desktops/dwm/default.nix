0e9a226df281ae4e3b7660634ad96a510f576c0e
you@example.com <you@example.com>
2024-09-06 12:35:55 +0200

    update

--- modules/desktops/dwm/default.nix
+++ modules/desktops/dwm/default.nix
@@ -1,29 +1,21 @@
-{ lib, nixpkgs, ... }:
+{ lib, ... }:
 
-let
-  system = "x86_64-linux";
-
-  pkgs = import nixpkgs {
-    inherit system;
-    config.allowUnfree = true;
-  };
-
-  lib = nixpkgs.lib;
-in
 {
-  xserver.enable = true;
-  xserver.displayManager.lightdm.enable = true;
-  services.xserver.windowManager.dwm = {
-    enable = true;
-    package = pkgs.dwm.overrideAttrs rec {
-      pname = "dwm";
-      version = "6.5";
-      src = pkgs.fetchFromGithub {
-      	owner = "Gekko-Geko";
-      	repo = "dwm";
-      	rev = "760824bedef2237f8eb45fcf554a3da4375b5c7e";
-      	hash = "";
-      };
-    };
+  services = {
+  	xserver.enable = true;
+  	xserver.displayManager.lightdm.enable = true;
+  	xserver.windowManager.dwm = {
+  	  enable = true;
+  	  package = pkgs.dwm.overrideAttrs rec {
+  	    pname = "dwm";
+  	    version = "6.5";
+  	    src = pkgs.fetchFromGithub {
+  	    	owner = "Gekko-Geko";
+  	    	repo = "dwm";
+  	    	rev = "760824bedef2237f8eb45fcf554a3da4375b5c7e";
+  	    	hash = "";
+  	    };
+  	  };
+  	};
   };
 }
