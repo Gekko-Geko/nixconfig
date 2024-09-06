4ecce8792078f4dbe997ca6e290398bf8113700a
you@example.com <you@example.com>
2024-09-06 13:45:34 +0200

    update

--- modules/programs/nvim.nix
+++ modules/programs/nvim.nix
@@ -2,12 +2,9 @@
 
 {
   programs = {
-  	# lua = { 
-	#   enable = true; 
-	# };
         neovim = {
           enable = true;
-          package = pkgs.neovim.overrideAttrs rec {
+          package = pkgs.neovim.overrideAttrs (oldAttr: {
             pname = "neovim";
             src = pkgs.fetchFromGitHub {
               owner = "Gekko-Geko";
@@ -15,13 +12,7 @@
               rev = "5d711d8ae5fc909f16fadba3341b857800b57f05";
               hash = "";
             };
-            # src = pkgs.fetchFromGitHub {
-            #   owner = "Gekko-Geko";
-            #   repo = "dwm";
-            #   rev = "760824bedef2237f8eb45fcf554a3da4375b5c7e";
-            #   hash = "sha256-4tiul2Ou8EzW3D9pKVEy1xuEPc2GoP2Vno5r+9hGzqI=";
-            # };
-          };
+          });
         };
   };
 }
