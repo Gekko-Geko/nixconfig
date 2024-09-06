e60f54f8fced617f07b14f4cb8fd2099d54fee2a
you@example.com <you@example.com>
2024-09-06 12:55:37 +0200

    update

--- modules/desktops/dwm/default.nix
+++ modules/desktops/dwm/default.nix
@@ -1,15 +1,15 @@
-{ lib, nixpkgs, ... }:
+{ lib, pkgs, ... }:
 
-let
-  system = "x86_64-linux";
-
-  pkgs = import nixpkgs {
-    inherit system;
-    config.allowUnfree = true;
-  };
-
-  lib = nixpkgs.lib;
-in
+# let
+#   system = "x86_64-linux";
+# 
+#   pkgs = import nixpkgs {
+#     inherit system;
+#     config.allowUnfree = true;
+#   };
+# 
+#   lib = nixpkgs.lib;
+# in
 {
   services = {
   	xserver.enable = true;
