8025042cb28e0f99bf2ba1e4323da879b071e460
you@example.com <you@example.com>
2024-09-09 09:35:52 +0200

    update

--- modules/desktops/dwm/default.nix
+++ modules/desktops/dwm/default.nix
@@ -28,7 +28,7 @@
   	    	owner = "Gekko-Geko";
   	    	repo = "dwm";
   	    	rev = "2bf4f8a985b6b8cdaa549746b694dada53aa722e";
-  	    	hash = "";
+  	    	hash = "sha256-+62UcSZ6oHJMSf/Jeob5H9TocaJR81ctKpcnQgGQWMA=";
   	    };
   	  };
   	};
