cf2a54ce3a6b88d6791435e79099de48ebd4e3c6
you@example.com <you@example.com>
2024-09-09 10:10:33 +0200

    update

--- modules/desktops/dwm/default.nix
+++ modules/desktops/dwm/default.nix
@@ -1,15 +1,5 @@
 { lib, pkgs, ... }:
 
-# let
-#   system = "x86_64-linux";
-# 
-#   pkgs = import nixpkgs {
-#     inherit system;
-#     config.allowUnfree = true;
-#   };
-# 
-#   lib = nixpkgs.lib;
-# in
 {
   services = {
   	xserver.enable = true;
@@ -18,12 +8,6 @@
   	  enable = true;
   	  package = pkgs.dwm.overrideAttrs rec {
   	    pname = "dwm";
-	    # src = pkgs.fetchFromGitHub {
-	    #          owner = "saloniamatteo";
-	    #          repo = "dwm";
-	    #          rev = "bb9b61617f40c46c441953758cf99e770ad02c55";
-	    #          hash = "sha256-4Yc2XHND3uiClaC9SgoukbWEV/Y2/XzSABXtuylacV0=";
-	    #        };
   	    src = pkgs.fetchFromGitHub {
   	    	owner = "Gekko-Geko";
   	    	repo = "dwm";
